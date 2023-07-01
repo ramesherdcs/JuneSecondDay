7f57c099ad44442ebdc2c808f86f639edf8d94f5
ramesherdcs <ramesherdcs@gmail.com>
2023-07-02 03:27:11 +0530

    A works

--- src/disp/com/TestingData.java
+++ src/disp/com/TestingData.java
@@ -3,7 +3,16 @@ package disp.com;
 public class TestingData {
 
 	public static void main(String[] args) {
+		String first="Java";
+		String second="Python";
+		String third="JavaScript";
+		System.out.println(first);
+		System.out.println(second);
+		System.out.println(third);
+		
 		System.out.println("Testing Successfully!!!!");
+		
+		
 
 	}
 
